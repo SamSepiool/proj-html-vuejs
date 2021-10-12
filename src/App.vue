51d0314c480e4842bddcfd3f2d91b9fd97284690
SamSepiool <84787501+SamSepiool@users.noreply.github.com>
2021-10-12 19:24:17 +0200

    scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <template>
   <div id="app">
 
+    <header>
+      <!-- LOGO -->
+
+      <nav>
+        <!-- UL MENU -->
+      </nav>
+    </header>
+
+    <main>
+      <!-- SECTION  untold stories-->
+      <!-- SECTION music is life-->
+
+      <section> <!-- latest brand news -->
+        <!-- CARD -->
+      </section>
+
+      <!-- SECTION review -->
+      <!-- SECTION live dates -->
+    </main>
+
+    <footer>
+      <!-- FOOTER-TOP -->
+      <!-- FOOTER-BOTTOM -->
+    </footer>
+
   </div>
 </template>
 
