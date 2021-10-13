a9ff9ea6c8e3c3f0c9b2cca10a4b08e1634b5f17
SamSepiool <84787501+SamSepiool@users.noreply.github.com>
2021-10-13 16:56:52 +0200

    add Main (latest_brand section) / add FullBtn component

---
+++ src/components/FullBtn.vue
@@ -0,0 +1,28 @@
+<template>
+  <a class="full_btn">VIEW ALL LATEST NEWS</a>
+</template>
+
+<script>
+export default {
+    name: 'FullBtn',
+}
+</script>
+
+<style lang='scss' scoped>
+@import '../assets/style/variables.scss';
+.full_btn{
+    text-decoration: none;
+    text-align: center;
+    display: block;
+    width: 100%;
+    padding: 3.125rem;
+    background-color: $primaryRed;
+    color: white;
+
+    &:hover{
+        background-color: white;
+        color:initial;
+    }
+}
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,103 +1,143 @@
 <template>
   <main>
-      <section class="hero text-white">
-
-          <div class="container-fluid h-100">
-              <div class="row h-100 text-center align-items-center" >
-                  <div class="col">
-                      <h1 class="fw-bold"> Untold Stories </h1>
-                      <p class="fs-3">There is an untold story behind every favorite song.</p>
-                      <div class="buttons ">
-                        <button type="button" class="fw-bold mx-3 px-5 py-3 btn btn-secondary">LATEST ALBUM</button>
-                        <button type="button" class="fw-bold mx-3 px-5 py-3 btn btn-outline-secondary">LIVE DATES</button>
-                      </div>
-                  </div>
-              </div>
+    <section class="hero text-white">
+      <div class="container-fluid h-100">
+        <div class="row h-100 text-center align-items-center">
+          <div class="col">
+            <h1 class="fw-bold">Untold Stories</h1>
+            <p class="fs-3">
+              There is an untold story behind every favorite song.
+            </p>
+            <div class="buttons">
+              <button
+                type="button"
+                class="fw-bold mx-3 px-5 py-3 btn btn-secondary"
+              >
+                LATEST ALBUM
+              </button>
+              <button
+                type="button"
+                class="fw-bold mx-3 px-5 py-3 btn btn-outline-secondary"
+              >
+                LIVE DATES
+              </button>
+            </div>
           </div>
-
-
-      </section>
-
-      <section class="is_life">
-          
-        <div class="container text-white">
-
-            <div class="row">
-                <div class="col">
-                    <div class="bookmark mx-auto my-0"></div>
-                </div>
-            </div>
-
-            <div class="row text-center py-5">
-                <div class="col">
-                    <h2 class="fs-1 fw-bold">Music is Life</h2>
-                    <hr class="mx-auto mb-4">
-                    <div class="box">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ipsa dolores rerum nostrum quam reiciendis voluptatem error animi, maiores adipisci sapiente vero magnam qui ut. Earum est totam ipsa impedit?</p>
-                    </div>    
-                </div>
-                    
-            </div>
         </div>
-          
-      </section>
+      </div>
+    </section>
 
+    <section class="is_life">
+      <div class="container text-white">
+        <div class="row">
+          <div class="col">
+            <div class="bookmark mx-auto my-0"></div>
+          </div>
+        </div>
+
+        <div class="row text-center py-5">
+          <div class="col">
+            <h2 class="fs-1 fw-bold">Music is Life</h2>
+            <hr class="mx-auto mb-4" />
+            <div class="box">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ipsa
+                dolores rerum nostrum quam reiciendis voluptatem error animi,
+                maiores adipisci sapiente vero magnam qui ut. Earum est totam
+                ipsa impedit?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="latest_brand">
+      <div class="container text-white">
+        <div class="row text-center py-5">
+          <div class="col">
+            <h2 class="fs-1 fw-bold">Latest Brand News</h2>
+            <hr class="mx-auto mb-4" />
+            <div class="box">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ipsa
+                dolores rerum nostrum quam reiciendis voluptatem error animi,
+                maiores adipisci sapiente vero magnam qui ut. Earum est totam
+                ipsa impedit?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container py-5">
+          
+       <!-- PIANO CARDS -->
+       <img class="w-100" src="http://www.tacchi.it/wp-content/uploads/2017/11/pp-work-in-progress-624x434.jpg" alt="">
+
+      </div>
+      <FullBtn/>
+      
+    </div>
   </main>
 </template>
 
 <script>
+import FullBtn from './FullBtn.vue'
 export default {
-    name:'Main',
-    components:{
-
-    }
-}
+  name: "Main",
+  components: {
+      FullBtn,
+  },
+};
 </script>
 
 <style lang='scss' scoped>
-@import '../assets/style/variables.scss';
+@import "../assets/style/variables.scss";
 // .row{
 //     border: 1px solid blue;
 // }
-.hero{
-    background-image: url(../assets/img/home_slider.jpg);
-    background-size: cover;
-    background-position:bottom;
-    height: 100vh;
-    
-    h1{
-        font-size: 9.375rem !important;
+.container{
+    border: 1px solid red;
+}
+.hero {
+  background-image: url(../assets/img/home_slider.jpg);
+  background-size: cover;
+  background-position: bottom;
+  height: 100vh;
+
+  h1 {
+    font-size: 9.375rem !important;
+  }
+
+  .buttons {
+    & button:first-child {
+      background-color: $primaryRed;
+      border: none;
+    }
+    & button:last-child {
+      border-color: white;
+      color: white;
     }
 
-    .buttons{
-        
-
-        & button:first-child{
-            background-color:$primaryRed;
-            border: none;
-        }
-        & button:last-child{
-            border-color: white;
-            color: white;
-        }
-
-        button:hover{
-            background-color: white;
-            color: black;
-        }
+    button:hover {
+      background-color: white;
+      color: black;
     }
-  
-    
+  }
 }
 
-.is_life{
-    background-color: $primaryGray;
+.is_life {
+  background-color: $primaryGray;
 
-    .bookmark{
-        background-color: $primaryRed;
-        height: .75rem;
-        width: 40%;
-    }
+  .bookmark {
+    background-color: $primaryRed;
+    height: 0.75rem;
+    width: 40%;
+  }
 }
 
+.latest_brand {
+  background-color: $secondaryGray;
+}
 </style>
