b8723edf5ac89540f26e53662c4bdf9c7f4f7505
SamSepiool <84787501+SamSepiool@users.noreply.github.com>
2021-10-15 17:07:51 +0200

    add accordions

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -71,70 +71,358 @@
       </div>
 
       <div class="container py-5">
-          
-       <!-- PIANO CARDS -->
-       <img class="w-100" src="http://www.tacchi.it/wp-content/uploads/2017/11/pp-work-in-progress-624x434.jpg" alt="">
-
+        <!-- PIANO CARDS -->
+        <img
+          class="w-100"
+          src="http://www.tacchi.it/wp-content/uploads/2017/11/pp-work-in-progress-624x434.jpg"
+          alt=""
+        />
       </div>
-      <FullBtn/>
-      
+      <FullBtn />
     </section>
 
     <section class="testimonial text-white">
-        <div class="container h-100">
+      <div class="container h-100">
         <div class="row h-100 text-center align-items-center">
           <div class="col">
             <div class="box">
-                <p class="fs-3 mb-5">
-                    "I just listened to it and said to myself, "Man, I really love this album."" Still, today, it just sounds so fresh. It sounds full of ideas. These guys knew what they were doing. They're good. And they're inventive I haven't heard anything this year that's as inventive I don't really expect to."
-                </p>
-                <a class="" href="#"><img src="../assets/img/band_interview_play_icon.png" alt=""></a>
-            </div>    
+              <p class="fs-3 mb-5">
+                "I just listened to it and said to myself, "Man, I really love
+                this album."" Still, today, it just sounds so fresh. It sounds
+                full of ideas. These guys knew what they were doing. They're
+                good. And they're inventive I haven't heard anything this year
+                that's as inventive I don't really expect to."
+              </p>
+              <a class="" href="#"
+                ><img src="../assets/img/band_interview_play_icon.png" alt=""
+              /></a>
+            </div>
           </div>
         </div>
       </div>
     </section>
 
     <section class="live_dates">
-
-        <div class="container text-white">
-            <div class="row text-center py-5">
-                <div class="col">
-                    <h2 class="fs-1 fw-bold">Live Dates</h2>
-                    <hr class="mx-auto mb-4" />
-                    <div class="box">
-                    <p class="section_text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ipsa
-                    dolores rerum nostrum quam reiciendis voluptatem error animi,
-                    maiores adipisci sapiente vero magnam qui ut. Earum est totam
-                    ipsa impedit?
-                    </p>
-                    </div>
-                </div>
+      <div class="container text-white">
+        <div class="row text-center py-5">
+          <div class="col">
+            <h2 class="fs-1 fw-bold">Live Dates</h2>
+            <hr class="mx-auto mb-4" />
+            <div class="box">
+              <p class="section_text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ipsa
+                dolores rerum nostrum quam reiciendis voluptatem error animi,
+                maiores adipisci sapiente vero magnam qui ut. Earum est totam
+                ipsa impedit?
+              </p>
             </div>
+          </div>
+        </div>
+      </div>
+
+      
+      <div class="container py-5">
+
+<!-- OPENING REQUIRES FIX-------------- OPENING REQUIRES FIX -->
+
+        <div class="accordion accordion-flush" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+              <button
+                class="accordion-button fw-bold"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                17/08/2020 GEM FESTIVAL 2020 ANAKALIA, GEORGIA
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              class="accordion-collapse collapse"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body d-flex">
+                <div class="map mx-3">
+                  <img
+                    src="http://www.tacchi.it/wp-content/uploads/2017/11/pp-work-in-progress-624x434.jpg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  class="
+                    mx-4
+                    d-flex
+                    flex-column
+                    align-items-start
+                    justify-content-between
+                  "
+                >
+                  <h4 class="py-3">Untold Stories</h4>
+
+                  <p class="pe-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+
+                  <button
+                    type="button"
+                    class="fw-bold px-3 py-2 btn btn-secondary"
+                  >
+                    BOOK NOW
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+              <button
+                class="accordion-button fw-bold"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                17/08/2020 GEM FESTIVAL 2020 ANAKALIA, GEORGIA
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              class="accordion-collapse collapse"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body d-flex">
+                <div class="map mx-3">
+                  <img
+                    src="http://www.tacchi.it/wp-content/uploads/2017/11/pp-work-in-progress-624x434.jpg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  class="
+                    mx-4
+                    d-flex
+                    flex-column
+                    align-items-start
+                    justify-content-between
+                  "
+                >
+                  <h4 class="py-3">Untold Stories</h4>
+
+                  <p class="pe-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+
+                  <button
+                    type="button"
+                    class="fw-bold px-3 py-2 btn btn-secondary"
+                  >
+                    BOOK NOW
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+              <button
+                class="accordion-button fw-bold"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                17/08/2020 GEM FESTIVAL 2020 ANAKALIA, GEORGIA
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              class="accordion-collapse collapse"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body d-flex">
+                <div class="map mx-3">
+                  <img
+                    src="http://www.tacchi.it/wp-content/uploads/2017/11/pp-work-in-progress-624x434.jpg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  class="
+                    mx-4
+                    d-flex
+                    flex-column
+                    align-items-start
+                    justify-content-between
+                  "
+                >
+                  <h4 class="py-3">Untold Stories</h4>
+
+                  <p class="pe-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+
+                  <button
+                    type="button"
+                    class="fw-bold px-3 py-2 btn btn-secondary"
+                  >
+                    BOOK NOW
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+              <button
+                class="accordion-button fw-bold"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                17/08/2020 GEM FESTIVAL 2020 ANAKALIA, GEORGIA
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              class="accordion-collapse collapse"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body d-flex">
+                <div class="map mx-3">
+                  <img
+                    src="http://www.tacchi.it/wp-content/uploads/2017/11/pp-work-in-progress-624x434.jpg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  class="
+                    mx-4
+                    d-flex
+                    flex-column
+                    align-items-start
+                    justify-content-between
+                  "
+                >
+                  <h4 class="py-3">Untold Stories</h4>
+
+                  <p class="pe-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+
+                  <button
+                    type="button"
+                    class="fw-bold px-3 py-2 btn btn-secondary"
+                  >
+                    BOOK NOW
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+              <button
+                class="accordion-button fw-bold"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                17/08/2020 GEM FESTIVAL 2020 ANAKALIA, GEORGIA
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              class="accordion-collapse collapse"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
+            >
+              <div class="accordion-body d-flex">
+                <div class="map mx-3">
+                  <img
+                    src="http://www.tacchi.it/wp-content/uploads/2017/11/pp-work-in-progress-624x434.jpg"
+                    alt=""
+                  />
+                </div>
+                <div
+                  class="
+                    mx-4
+                    d-flex
+                    flex-column
+                    align-items-start
+                    justify-content-between
+                  "
+                >
+                  <h4 class="py-3">Untold Stories</h4>
+
+                  <p class="pe-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+
+                  <button
+                    type="button"
+                    class="fw-bold px-3 py-2 btn btn-secondary"
+                  >
+                    BOOK NOW
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div class="container py-5">
+<!-- OPENING REQUIRES FIX-------------- OPENING REQUIRES FIX -->
 
-            <img class="w-100" src="http://www.tacchi.it/wp-content/uploads/2017/11/pp-work-in-progress-624x434.jpg" alt="">
+      </div>
 
-        </div>
-
-        <FullBtn/>
-
-
+      <FullBtn />
     </section>
-
-    
   </main>
 </template>
 
 <script>
-import FullBtn from './FullBtn.vue'
+import FullBtn from "./FullBtn.vue";
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// import { faPlus as faPlus  } from '@fortawesome/free-solid-svg-icons';
 export default {
   name: "Main",
   components: {
-      FullBtn,
+    FullBtn,
+    // FontAwesomeIcon
+  },
+  data() {
+    return {};
   },
 };
 </script>
@@ -175,18 +463,17 @@ export default {
   background-color: $secondaryGray;
 
   .bookmark {
-  background-color: $primaryRed;
-  height: 0.75rem;
-  width: 40%;
+    background-color: $primaryRed;
+    height: 0.75rem;
+    width: 40%;
   }
-
 }
 
 .latest_brand {
   background-color: $primaryGray;
 }
 
-.testimonial{
+.testimonial {
   background-image: url(../assets/img/home-testimonial-parallax.jpg);
   height: 80vh;
   background-attachment: fixed;
@@ -194,15 +481,52 @@ export default {
   background-repeat: no-repeat;
   background-size: cover;
 
-  p{
+  p {
     font-style: italic;
   }
-
 }
 
-.live_dates{
-    background-color: $primaryGray;
+.live_dates {
+  background-color: $primaryGray;
+
+  .accordion {
+    .accordion-item {
+      border: none;
+      margin-bottom: 10px;
+    }
+
+    .accordion-button {
+      background-color: $secondaryGray;
+      color: $primaryRed;
+      font-size: bold;
+    }
+    .accordion-button::after {
+      content: none;
+    }
+
+    .accordion-body {
+      background-color: $secondaryGray;
+      img {
+        width: 300px;
+        border: 1px solid red;
+        margin-right: 20px;
+      }
+      button {
+        background-color: $primaryRed;
+        border: none;
+
+        &:hover {
+          background-color: white;
+          color: black;
+        }
+      }
+      h4 {
+        color: $white;
+      }
+      p {
+        color: $gray;
+      }
+    }
+  }
 }
-
-
 </style>
